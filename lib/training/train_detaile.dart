8052b1447591ba8b8fb004988e8e6c9a8a0f9c94
Tolibzhon <sidikovtolibjon2000@gmail.com>
2024-03-17 16:41:45 +0600

    add change

---
+++ lib/training/train_detaile.dart
@@ -0,0 +1,194 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:familyforge_fitness_130/core/ff_colors.dart';
+import 'package:familyforge_fitness_130/training/training_rasp.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+
+class TrainDetaile extends StatelessWidget {
+  const TrainDetaile({super.key, required this.model, required this.index});
+  final Rasp model;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        leading: const BackButton(color: FFColors.grey555555),
+        title: Text(
+          'Exercise $index',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24.h,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 10.h),
+            ClipRRect(
+              child: CachedNetworkImage(
+                imageUrl: model.image,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                placeholder: (context, url) => ClipRRect(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Shimmer.fromColors(
+                      baseColor: Colors.grey.shade500,
+                      highlightColor: Colors.grey.shade200,
+                      child: Container(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
+            SizedBox(height: 16.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    model.backExercises,
+                    style: TextStyle(
+                      color: const Color(0xffE80000),
+                      fontSize: 13.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    model.title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+                  Text(
+                    'BODY PART:',
+                    style: TextStyle(
+                      color: const Color(0xffE80000),
+                      fontSize: 13.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    model.bodyPart,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+                  Text(
+                    'EQUIPMENT:',
+                    style: TextStyle(
+                      color: const Color(0xffE80000),
+                      fontSize: 13.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    model.equipment,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+                  Text(
+                    'DIFFICULTY:',
+                    style: TextStyle(
+                      color: const Color(0xffE80000),
+                      fontSize: 13.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    model.difficulty,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const Divider(
+                    height: 32,
+                    color: Color(0xff222222),
+                  ),
+                  Text(
+                    'Step 1',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    model.step1,
+                    style: TextStyle(
+                      color: FFColors.grey555555,
+                      fontSize: 14.h,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const Divider(
+                    height: 32,
+                    color: Color(0xff222222),
+                  ),
+                  Text(
+                    'Step 2',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    model.step2,
+                    style: TextStyle(
+                      color: FFColors.grey555555,
+                      fontSize: 14.h,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const Divider(
+                    height: 32,
+                    color: Color(0xff222222),
+                  ),
+                  Text(
+                    'Step 3',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    model.step3,
+                    style: TextStyle(
+                      color: FFColors.grey555555,
+                      fontSize: 14.h,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 35.h),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/training/training_screen.dart
+++ lib/training/training_screen.dart
@@ -1,12 +1,24 @@
+import 'package:familyforge_fitness_130/core/ff_motin.dart';
+import 'package:familyforge_fitness_130/training/training_rasp.dart';
+import 'package:familyforge_fitness_130/training/widgets/for_widget.dart';
+import 'package:familyforge_fitness_130/training/widgets/train_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class TrainingScreen extends StatelessWidget {
+class TrainingScreen extends StatefulWidget {
   const TrainingScreen({super.key});
+
+  @override
+  State<TrainingScreen> createState() => _TrainingScreenState();
+}
+
+class _TrainingScreenState extends State<TrainingScreen> {
+  String page = 'For Parents';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        scrolledUnderElevation: 0,
         title: Text(
           'Training',
           style: TextStyle(
@@ -17,10 +29,109 @@ class TrainingScreen extends StatelessWidget {
         ),
         centerTitle: false,
       ),
-      body: Column(
-        children: [
-          Container(),
-        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.r),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(2),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: const Color(0xff767680).withOpacity(0.24),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: FFMotion(
+                      onPressed: () {
+                        setState(() {
+                          page = 'For Parents';
+                        });
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 28.h,
+                        decoration: BoxDecoration(
+                          color: page == 'For Parents'
+                              ? const Color(0xff636366)
+                              : null,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'For Parents',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.h,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FFMotion(
+                      onPressed: () {
+                        setState(() {
+                          page = 'For Kids';
+                        });
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 28.h,
+                        decoration: BoxDecoration(
+                          color: page == 'For Kids'
+                              ? const Color(0xff636366)
+                              : null,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'For Kids',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.h,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 6.h),
+            page == 'For Parents'
+                ? Expanded(
+                    child: ListView.separated(
+                      padding: EdgeInsets.symmetric(vertical: 10.r),
+                      shrinkWrap: true,
+                      itemCount: listTraining.length,
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 12),
+                      itemBuilder: (context, index) => TrainWidget(
+                        model: listTraining[index],
+                        index: index + 1,
+                      ),
+                    ),
+                  )
+                : Expanded(
+                    child: ListView.separated(
+                      padding: EdgeInsets.symmetric(vertical: 10.r),
+                      shrinkWrap: true,
+                      itemCount: listVideo.length,
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 12),
+                      itemBuilder: (context, index) => ForWidget(
+                        model: listVideo[index],
+                      ),
+                    ),
+                  ),
+          ],
+        ),
       ),
     );
   }
